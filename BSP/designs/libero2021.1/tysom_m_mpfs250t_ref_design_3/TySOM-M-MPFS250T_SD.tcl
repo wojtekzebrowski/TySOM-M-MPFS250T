978aed9c42f5304cc243c3e018d26e9ada201680
Piotr Czak <piotrc@aldec.com>
2021-06-24 08:34:30 +0200

    Updated reference design 3

---
+++ BSP/designs/libero2021.1/tysom_m_mpfs250t_ref_design_3/TySOM-M-MPFS250T_SD.tcl
@@ -0,0 +1,72 @@
+# TySOM-M-MPFS250T SD Libero design
+
+if {[string compare [string range [get_libero_version] 0 end-3] "2021.1.0"]==0} { 
+	puts "Libero v2021.1 detected." 
+} else {
+	error "Incorrect Libero version. Please use Libero v2021.1 to run these scripts."
+}
+
+if { [lindex $tcl_platform(os) 0]  == "Windows" } {
+	if {[string length [pwd]] < 90} {
+		puts "Project path length ok." 
+	} else {
+		error "Path to project is too long, please reduce the path and try again."
+	}
+}
+
+if { $::argc > 0 } {
+    set i 1
+    foreach arg $::argv {
+		set temp [split $arg ":"]
+		puts "Setting parameter [lindex $temp 0] to [lindex $temp 1]"
+		set [lindex $temp 0] "[lindex $temp 0]:[lindex $temp 1]"
+        incr i
+    }
+} else {
+    puts "no command line argument passed"
+}
+
+set install_loc [defvar_get -name ACTEL_SW_DIR]
+set mss_config_loc "$install_loc/bin64/pfsoc_mss"
+set local_dir [pwd]
+set project_dir "$local_dir/TySOM_M_MPFS250T_SD"
+
+set emmc_sd "sd"
+
+set constraint_path ./constraints
+
+new_project -location {./TySOM_M_MPFS250T_SD} -name {TySOM_M_MPFS250T_SD} -project_description {} -block_mode 0 -standalone_peripheral_initialization 0 -instantiate_in_smartdesign 1 -ondemand_build_dh 1 -use_relative_path 0 -linked_files_root_dir_env {} -hdl {VERILOG} -family {PolarFireSoC} -die {MPFS250T_ES} -package {FCG1152} -speed {-1} -die_voltage {1.0} -part_range {EXT} -adv_options {IO_DEFT_STD:LVCMOS 1.8V} -adv_options {RESTRICTPROBEPINS:1} -adv_options {RESTRICTSPIPINS:0} -adv_options {SYSTEM_CONTROLLER_SUSPEND_MODE:0} -adv_options {TEMPR:EXT} -adv_options {VCCI_1.2_VOLTR:EXT} -adv_options {VCCI_1.5_VOLTR:EXT} -adv_options {VCCI_1.8_VOLTR:EXT} -adv_options {VCCI_2.5_VOLTR:EXT} -adv_options {VCCI_3.3_VOLTR:EXT} -adv_options {VOLTR:EXT} 
+set_device_simple -family {PolarFireSoC} -die {MPFS250T_ES} -package {FCG1152} -speed {-1} -die_voltage {1.0} -part_range {EXT} -adv_options {IO_DEFT_STD:LVCMOS 1.8V} -adv_options {RESERVEMIGRATIONPINS:1} -adv_options {RESTRICTPROBEPINS:0} -adv_options {RESTRICTSPIPINS:0} -adv_options {SYSTEM_CONTROLLER_SUSPEND_MODE:0} -adv_options {TEMPR:EXT} -adv_options {VCCI_1.2_VOLTR:EXT} -adv_options {VCCI_1.5_VOLTR:EXT} -adv_options {VCCI_1.8_VOLTR:EXT} -adv_options {VCCI_2.5_VOLTR:EXT} -adv_options {VCCI_3.3_VOLTR:EXT} -adv_options {VOLTR:EXT} 
+
+download_core -vlnv {Actel:SgCore:PF_OSC:1.0.102} -location {www.microchip-ip.com/repositories/SgCore}
+download_core -vlnv {Actel:SgCore:PF_CCC:2.2.100} -location {www.microchip-ip.com/repositories/SgCore}
+download_core -vlnv {Actel:DirectCore:CORERESET_PF:2.3.100} -location {www.microchip-ip.com/repositories/DirectCore}
+download_core -vlnv {Microsemi:SgCore:PFSOC_INIT_MONITOR:1.0.205} -location {www.microchip-ip.com/repositories/SgCore}
+download_core -vlnv {Actel:DirectCore:COREAXI4INTERCONNECT:2.8.103} -location {www.microchip-ip.com/repositories/DirectCore}
+download_core -vlnv {Actel:SgCore:PF_CLK_DIV:1.0.103} -location {www.microchip-ip.com/repositories/SgCore}
+download_core -vlnv {Actel:SgCore:PF_DRI:1.1.100} -location {www.microchip-ip.com/repositories/SgCore}
+download_core -vlnv {Actel:SgCore:PF_NGMUX:1.0.101} -location {www.microchip-ip.com/repositories/SgCore}
+download_core -vlnv {Actel:SgCore:PF_PCIE:2.0.104} -location {www.microchip-ip.com/repositories/SgCore}
+download_core -vlnv {Actel:SgCore:PF_TX_PLL:2.0.300} -location {www.microchip-ip.com/repositories/SgCore}
+download_core -vlnv {Actel:SgCore:PF_XCVR_REF_CLK:1.0.103} -location {www.microchip-ip.com/repositories/SgCore}
+download_core -vlnv {Actel:DirectCore:CoreAPB3:4.2.100} -location {www.microchip-ip.com/repositories/DirectCore}
+download_core -vlnv {Actel:Simulation:CLK_GEN:1.0.1} -location {www.microchip-ip.com/repositories/SgCore}
+download_core -vlnv {Actel:Simulation:RESET_GEN:1.0.1} -location {www.microchip-ip.com/repositories/SgCore}
+download_core -vlnv {Actel:SystemBuilder:PF_DDR4:2.5.102} -location {www.actel-ip.com/repositories/SgCore}
+
+source ./script_support/TySOM_M_MPFS250T_SD_recursive.tcl
+
+run_tool -name {CONSTRAINT_MANAGEMENT} 
+
+# Import I/O constraints
+import_files \
+         -convert_EDN_to_HDL 0 \
+         -io_pdc "${constraint_path}/io.pdc" \
+	 -io_pdc "${constraint_path}/ALDEC_DDR4.pdc"
+	 
+import_files -convert_EDN_to_HDL 0 -fp_pdc "${constraint_path}/CCC.pdc"
+
+organize_tool_files -tool {PLACEROUTE} -file "${project_dir}/constraint/io/io.pdc" -file "${project_dir}/constraint/io/ALDEC_DDR4.pdc" -file "${project_dir}/constraint/fp/CCC.pdc" -module {tysom_m_v10::work} -input_type {constraint} 
+
+derive_constraints_sdc 
+save_project 
---
+++ BSP/designs/libero2021.1/tysom_m_mpfs250t_ref_design_3/script_support/TySOM_M_MPFS250T_SD_recursive.tcl
@@ -0,0 +1,25 @@
+build_design_hierarchy
+
+import_mss_component -file "$local_dir/script_support/components/MSS_SD/Aldec_MSSv2_sd.cxz"
+
+source script_support/components/CLK_125MHz_to_CLK_62_5MHz.tcl 
+source script_support/components/CLK_160MHz_to_CLK_80MHz.tcl 
+source script_support/components/FIC0_MASTER.tcl 
+source script_support/components/FIC1_MASTER.tcl 
+source script_support/components/FIC3_MASTER.tcl 
+source script_support/components/GLITCHLESS_MUX.tcl 
+source script_support/components/OSCILLATOR_160MHz.tcl 
+source script_support/components/PCIE_MASTER.tcl 
+source script_support/components/PCIE_REF_CLK.tcl 
+source script_support/components/PFSOC_INIT_MONITOR_C0.tcl 
+source script_support/components/PF_CCC_C1.tcl 
+source script_support/components/PF_DDR4_C0.tcl 
+source script_support/components/PF_PCIE_C0.tcl 
+source script_support/components/RECONFIGURATION_INTERFACE.tcl 
+source script_support/components/RESET_CLK_125MHz.tcl 
+source script_support/components/RESET_CLK_62_5MHz.tcl 
+source script_support/components/TRANSMIT_PLL.tcl 
+source script_support/components/tysom_m_v10_sd.tcl 
+build_design_hierarchy
+
+set_root -module {tysom_m_v10::work} 
